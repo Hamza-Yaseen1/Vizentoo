1554b0a35a55dc5a16e2dc39a136cb54feac2ec8
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-03-13 18:30:43 +0500

    Refactor Hero2 component

--- src/app/Components/hero2.tsx
+++ src/app/Components/hero2.tsx
@@ -1,32 +1,35 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-function Hero2(){
-    return(
-        <>
-        <section className="md:flex justify-evenly p-10 pt-28">
-            <div className="text md:w-1/2 "> 
-            <h1 className="text-3xl font-bold p-3">Solve Your Design
-            barrier Today</h1>
+function Hero2() {
+  return (
+    <>
+      <section className="md:flex justify-evenly p-9 pt-24">
+        <div className="text md:w-1/2 ">
+          <h1 className="text-3xl font-bold p-3">
+            Solve Your Design barrier Today
+          </h1>
 
-            <p className="text-md text-gray-500 p-3">Your brand&apos;s design deserves more than unreliable freelancers
-or costly in-house teams. Hire
-
-a dedicated, experienced designer who understands both your
-brand and your vision, without the high costs and profit cuts of
-maintaining a full-time team</p>
-            <button className="rounded-2xl px-4 py-2 border border-black">Click me</button>
-            </div>
-            <div className="Image-hero">
-                <Image
-                src="/images/pexels-photo-11180937.webp"
-                alt="Description"
-                height={300}
-                width={300}
-                className='pt-10'
-                />
-            </div>
-        </section>
-        </>
-    )
+          <p className="text-md text-gray-500 p-3">
+            Your brand&apos;s design deserves more than unreliable freelancers
+            or costly in-house teams. Hire a dedicated, experienced designer who
+            understands both your brand and your vision, without the high costs
+            and profit cuts of maintaining a full-time team
+          </p>
+          <button className="rounded-2xl mt-4 px-4 py-2 border border-black hover:bg-black hover:text-white cursor-pointer">
+            Click me
+          </button>
+        </div>
+        <div className="Image-hero">
+          <Image
+            src="/images/pexels-photo-11180937.webp"
+            alt="Description"
+            height={300}
+            width={300}
+            className="pt-10"
+          />
+        </div>
+      </section>
+    </>
+  );
 }
 export default Hero2;
